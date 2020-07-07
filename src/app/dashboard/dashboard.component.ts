bc3fe5bb6bdaa27d1d6638c3f29d6f15e70f3d29
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-07-08 02:30:28 +0700

    done without styling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,8 @@ import { Injectable } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   private climates: any;
+  private date_list: any;
+  public formatted_climates: any;
 
   constructor( private http: HttpClient ) { }
 
@@ -34,23 +36,95 @@ export class DashboardComponent implements OnInit {
     this.getClimate(city).subscribe( res => {
       if (res) {
         this.climates = res['list'];
-        this.formatingClimate(this.climates)
-        console.log(this.climates)
+        this.date_list = this.getDateList(this.climates);
+        this.formatted_climates = this.formatingClimates(this.climates, this.date_list);
       }
     })
 
   }
 
   // fromating climate
-  formatingClimate(climates){
-    let date_now = new Date()
-    console.log('testt',date_now)
-    // for (let i = 0; i < climatest)
+  formatingClimates(climates, date_list){
+
+    let formatted_climates = [];
+    let divided_data = this.dividingData(climates, date_list);
+    for (let i = 0; i < this.date_list.length; i++) {
+      let climate_object = {
+        date: date_list[i],
+        climate: this.getAverageClimate(divided_data[date_list[i]]),
+        diff: this.getAverageDiffClimate(divided_data[date_list[i]])
+      }
+      formatted_climates.push(climate_object);
+    }
+    console.log('divided', formatted_climates);
+    return formatted_climates;
+
+  }
+
+  // get average climate
+  getAverageClimate(data) {
+
+    let climate = 0;
+    for (let i = 0; i < data.length; i++) {
+      climate += data[i].main.temp;
+    }
+    climate = climate / data.length;
+    let celcius_climates = (climate - 273.15).toFixed(2);
+    return celcius_climates;
+
+  }
+
+  // get average diff climate
+  getAverageDiffClimate(data) {
+
+    let climate_diff = 0;
+    for (let i = 0; i < data.length; i++) {
+      climate_diff += (data[i].main.temp_max - data[i].main.temp_min);
+    }
+    climate_diff = climate_diff / data.length;
+    let celcius_climates_diff = climate_diff.toFixed(2);
+    return celcius_climates_diff;
+
+  }
+
+  // get date list next 5 days
+  getDateList(climates) {
+
+    let date_list = []
+    for (let i = 0; i < climates.length; i++) {
+      let date = climates[i].dt_txt.slice(0,10)
+      if (date_list.indexOf(date) < 0 && date_list.length < 5) {
+        date_list.push(date)
+      }
+    }
+    return date_list;
+
+  }
+
+  // dividing all data to 5 days
+  dividingData(climates, date_list) {
+
+    let data_per_day = {}
+
+    for (let i = 0; i < date_list.length; i++) {
+      let temp_data_per_day = []
+
+      for (let j = 0; j < climates.length; j++) {
+        if (date_list[i] == climates[j].dt_txt.slice(0,10)) {
+          temp_data_per_day.push(climates[j])
+        }
+      }
+
+      data_per_day[date_list[i]] = temp_data_per_day
+    }
+
+    return data_per_day
   }
 
   cityChanges(event) {
-    console.log(typeof event.srcElement.value, event.srcElement.value)
-    let city = event.srcElement.value
+
+    let city = event.srcElement.value;
     this.setClimate(city);
+
   }
 }
